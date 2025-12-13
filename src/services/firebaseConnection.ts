d7661454c1d64ffb3a7bcdfdca908c38821a6863
Gabriel <gabriel.mouramagsk7@gmail.com>
2025-12-12 12:12:27 -1200

    Atualização completa: Tailwind v4, arrumado build, tsconfig e estrutura

--- src/services/firebaseConnection.ts
+++ src/services/firebaseConnection.ts
@@ -1,20 +1,18 @@
-
-
-import { initializeApp } from "firebase/app";
-import {getFirestore} from 'firebase/firestore'
-import { getAuth } from "firebase/auth";
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAhxp6zOhUc-xWhH7OCMZ7HTEEkNMbB5jk",
-  authDomain: "reactlinks-7377a.firebaseapp.com",
-  projectId: "reactlinks-7377a",
-  storageBucket: "reactlinks-7377a.firebasestorage.app",
-  messagingSenderId: "347733440490",
-  appId: "1:347733440490:web:b3cfbc825c9cd7eff7e493"
-};
+  apiKey: 'AIzaSyAhxp6zOhUc-xWhH7OCMZ7HTEEkNMbB5jk',
+  authDomain: 'reactlinks-7377a.firebaseapp.com',
+  projectId: 'reactlinks-7377a',
+  storageBucket: 'reactlinks-7377a.firebasestorage.app',
+  messagingSenderId: '347733440490',
+  appId: '1:347733440490:web:b3cfbc825c9cd7eff7e493',
+}
 
-
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig)
 const auth = getAuth(app)
-const db = getFirestore(app);
-export { auth, db }; 
+const db = getFirestore(app)
+
+export { auth, db } 
